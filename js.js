d53db62642e0d4c446db6a75cdd19bcaf79b1718
Игорь Козловский <i.kozlovsky2004@gmail.com>
2023-01-05 22:40:36 +0200

    Update js.js

--- js.js
+++ js.js
@@ -6,3 +6,15 @@
 // removeElement(array, 5 );
 // console.log(array);
 // Результат: [1, 2, 3, 4, 6, 7]
+
+const array = [1, 2, 3, 4, 98, 5, 6, 7];
+
+const removeElementByIndex = (array, index) => {
+  return array.filter((e, i) => i !== index - 1);
+};
+const removeElementByValue = (array, value) => {
+  return array.filter((e) => e !== value);
+};
+console.log("Удаление по индексу: ", removeElementByIndex(array, 6));
+console.log("Удаление по значению: ", removeElementByValue(array, 98));
+console.log("Начальный массив: ", array);
